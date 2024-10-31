90108d2540db7710e915af3726525aa345e37748
RyanMatthews3 <RYAN.MATHEWS12@student.qcc.cuny.edu>
2024-10-31 16:10:44 -0400

    updated files

---
+++ project1/events.js
@@ -0,0 +1,15 @@
+function showColorInfo() {
+    alert("Colors are everywhere! Let's learn about red, blue, green, and other colors");
+}
+
+function showNumberInfo() {
+    alert("Numbers help us count. Let's learn to count from 1 to 10");
+}
+
+function showAnimalInfo() {
+    alert("Animals are interesting  Let's learn about cats, dogs, elephants, and more!");
+}
+
+function showTransportationInfo() {
+    alert("WHats your favorite way to travel? ");
+}
